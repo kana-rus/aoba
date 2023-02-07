76908a0e5bccef73c076b3b5d8a283e368f0753d
kana-rus <kanarus786@gmail.com>
2023-02-07 19:14:05 +0900

    Update: experiment::user

--- src/experiment/schema.rs
+++ src/experiment/schema.rs
@@ -10,6 +10,8 @@ schema! {
 
 use super::aoba;
 
+use self::__private::{UserDeleter, UserUpdater, UserCreater, UserSelecter};
+
 #[allow(unused)]
 pub struct User {
     id: Option<u32>,
@@ -22,27 +24,27 @@ pub struct User {
         NameStr: aoba::AsStr,
         PasswordStr: aoba::AsStr,
     >(user: CreateUser<NameStr, PasswordStr>) -> UserCreater<NameStr, PasswordStr> {
-        UserCreater(user)
+        UserCreater::new(user)
     }
     #[allow(non_snake_case)]
     #[allow(unused)]
     pub fn FIRST() -> UserSelecter {
-        UserSelecter { select1: true, limit: None, order: None, condition: UserCondition::new() }
+        UserSelecter::new(Some(1))
     }
     #[allow(non_snake_case)]
     #[allow(unused)]
     pub fn ALL() -> UserSelecter {
-        UserSelecter { select1: false, limit: None, order: None, condition: UserCondition::new() }
+        UserSelecter::new(None)
     }
     #[allow(non_snake_case)]
     #[allow(unused)]
     pub fn UPDATE() -> UserUpdater {
-        UserUpdater { set_values: UpdateUser::new(), limit: None, order: None, condition: UserCondition::new() }
+        UserUpdater::new()
     }
     #[allow(non_snake_case)]
     #[allow(unused)]
     pub fn DELETE() -> UserDeleter {
-        UserDeleter { limit: None, order: None, condition: UserCondition::new() }
+        UserDeleter::new()
     }
 }
 
@@ -51,270 +53,291 @@ pub struct CreateUser<
     NameStr: aoba::AsStr,
     PasswordStr: aoba::AsStr,
 > {
-    id: u32,
-    name: NameStr,
-    password: PasswordStr,
+    pub name: NameStr,
+    pub password: PasswordStr,
 }
 
-#[allow(unused)]
-pub struct UpdateUser {
-    id: Option<u32>,
-    name: Option<String>,
-    password: Option<String>,
-}
-impl UpdateUser {
-    #[inline]
-    fn new() -> Self {
-        Self { id: None, name: None, password: None }
-    }
-}
-impl UpdateUser {
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn id(mut self, id: u32) -> Self {
-        self.id = Some(id);
-        self
-    }
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn name<Str: aoba::AsString>(mut self, name: Str) -> Self {
-        self.name = Some(name.as_string());
-        self
-    }
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn password<Str: aoba::AsString>(mut self, password: Str) -> Self {
-        self.password = Some(password.as_string());
-        self
-    }
-}
 
-#[allow(unused)]
-pub struct UserFields {
-    id: UserField,
-    name: UserField,
-    password: UserField,
-}
+pub mod __private {
+    use crate::experiment::aoba;
+    use super::CreateUser;
 
-#[allow(non_camel_case_types)]
-#[allow(unused)]
-pub enum UserField {
-    id,
-    name,
-    password,
-} impl UserField {
-    fn as_field_name(self) -> &'static str {
-        match self {
-            Self::id => "id",
-            Self::name => "name",
-            Self::password => "password",
+    #[allow(unused)]
+    pub struct UpdateUser {
+        id: Option<u32>,
+        name: Option<String>,
+        password: Option<String>,
+    }
+    impl UpdateUser {
+        #[inline]
+        pub(super) fn new() -> Self {
+            Self { id: None, name: None, password: None }
         }
     }
-}
-
-#[allow(unused)]
-pub struct UserCondition(String);
-impl UserCondition {
-    #[inline]
-    fn new() -> Self {
-        Self(String::from("WHERE"))
+    impl UpdateUser {
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn name<Str: aoba::AsString>(mut self, name: Str) -> Self {
+            self.name = Some(name.as_string());
+            self
+        }
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn password<Str: aoba::AsString>(mut self, password: Str) -> Self {
+            self.password = Some(password.as_string());
+            self
+        }
     }
 
-    #[inline]
-    fn is_empty(&self) -> bool {
-        self.0.len() == 5
+    #[allow(unused)]
+    pub struct UserFields {
+        pub id: UserField,
+        pub name: UserField,
+        pub password: UserField,
     }
 
-    #[inline]
-    fn and(mut self, new_condition: String) -> Self {
-        self.0 += if self.is_empty() {" AND "} else {" "};
-        self.0 += &new_condition;
-        self
-    }
-}
-impl UserCondition {
-    #[allow(non_snake_case)]
+    #[allow(non_camel_case_types)]
     #[allow(unused)]
-    pub fn id_eq(mut self, another: u32) -> Self {
-        self.and(format!("id = {another}"))
-    }
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn id_between(mut self, left: u32, right: u32) -> Self {
-        self.and(format!("id BETWEEN {left} AND {right}"))
+    pub enum UserField {
+        id,
+        name,
+        password,
+    } impl UserField {
+        fn as_field_name(self) -> &'static str {
+            match self {
+                Self::id => "id",
+                Self::name => "name",
+                Self::password => "password",
+            }
+        }
     }
 
-    #[allow(non_snake_case)]
     #[allow(unused)]
-    pub fn name_eq<Str: aoba::AsStr>(mut self, another: Str) -> Self {
-        self.and(format!("name = '{}'", another.as_str()))
-    }
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn name_like<Str: aoba::AsStr>(mut self, regex: Str) -> Self {
-        self.and(format!("name LIKE '{}'", regex.as_str()))
-    }
-    
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn password_eq<Str: aoba::AsStr>(mut self, another: Str) -> Self {
-        self.and(format!("password = '{}'", another.as_str()))
-    }
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn password_like<Str: aoba::AsStr>(mut self, regex: Str) -> Self {
-        self.and(format!("password LIKE '{}'", regex.as_str()))
-    }
-}
+    pub struct UserCondition(String);
+    impl UserCondition {
+        #[inline]
+        pub(super) fn new() -> Self {
+            Self(String::from("WHERE"))
+        }
 
-#[allow(unused)]
-pub struct UserCreater<
-    NameStr: aoba::AsStr,
-    PasswordStr: aoba::AsStr,
->(CreateUser<NameStr, PasswordStr>);
-impl<
-    NameStr: aoba::AsStr,
-    PasswordStr: aoba::AsStr,
-> UserCreater<NameStr, PasswordStr> {
-    // pub async fn exec<'e, E: sqlx::Executor<'e>>(self, executor: E) -> 
-}
+        #[inline]
+        fn is_empty(&self) -> bool {
+            self.0.len() == 5
+        }
 
-#[allow(unused)]
-pub struct UserSelecter {
-    select1:   bool,
-    limit:     Option<usize>,
-    order:     Option<aoba::Order>,
-    condition: UserCondition,
-} impl UserSelecter {
-    // ======
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn WHERE<F: Fn(UserCondition) -> UserCondition>(mut self, condition_builder: F) -> Self {
-        self.condition = condition_builder(self.condition);
-        self
+        #[inline]
+        fn and(mut self, new_condition: String) -> Self {
+            self.0 += if self.is_empty() {" AND "} else {" "};
+            self.0 += &new_condition;
+            self
+        }
     }
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn LIMIT(mut self, limit: usize) -> Self {
-        self.limit = Some(limit);
-        self
-    }
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn ORDER_ASC<F: Fn(UserFields) -> UserField>(mut self, field_selecter: F) -> Self {
-        let field = field_selecter(UserFields {
-            id: UserField::id,
-            name: UserField::name,
-            password: UserField::password,
-        });
-        self.order = Some(aoba::Order::Asc(field.as_field_name()));
-        self
-    }
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn ORDER_DESC<F: Fn(UserFields) -> UserField>(mut self, field_selecter: F) -> Self {
-        let field = field_selecter(UserFields {
-            id: UserField::id,
-            name: UserField::name,
-            password: UserField::password,
-        });
-        self.order = Some(aoba::Order::Desc(field.as_field_name()));
-        self
-    }
-    // =====
-}
+    impl UserCondition {
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn id_eq(mut self, another: u32) -> Self {
+            self.and(format!("id = {another}"))
+        }
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn id_between(mut self, left: u32, right: u32) -> Self {
+            self.and(format!("id BETWEEN {left} AND {right}"))
+        }
 
-#[allow(unused)]
-pub struct UserUpdater {
-    set_values: UpdateUser,
-    limit:      Option<usize>,
-    order:      Option<aoba::Order>,
-    condition:  UserCondition,
-} impl UserUpdater {
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn VALUES<F: Fn(UpdateUser) -> UpdateUser>(mut self, values_setter: F) -> Self {
-        self.set_values = values_setter(self.set_values);
-        self
-    }
-    // ======
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn WHERE<F: Fn(UserCondition) -> UserCondition>(mut self, condition_builder: F) -> Self {
-        self.condition = condition_builder(self.condition);
-        self
-    }
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn LIMIT(mut self, limit: usize) -> Self {
-        self.limit = Some(limit);
-        self
-    }
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn ORDER_ASC<F: Fn(UserFields) -> UserField>(mut self, field_selecter: F) -> Self {
-        let field = field_selecter(UserFields {
-            id: UserField::id,
-            name: UserField::name,
-            password: UserField::password,
-        });
-        self.order = Some(aoba::Order::Asc(field.as_field_name()));
-        self
-    }
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn ORDER_DESC<F: Fn(UserFields) -> UserField>(mut self, field_selecter: F) -> Self {
-        let field = field_selecter(UserFields {
-            id: UserField::id,
-            name: UserField::name,
-            password: UserField::password,
-        });
-        self.order = Some(aoba::Order::Desc(field.as_field_name()));
-        self
-    }
-    // =====
-}
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn name_eq<Str: aoba::AsStr>(mut self, another: Str) -> Self {
+            self.and(format!("name = '{}'", another.as_str()))
+        }
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn name_like<Str: aoba::AsStr>(mut self, regex: Str) -> Self {
+            self.and(format!("name LIKE '{}'", regex.as_str()))
+        }
 
-#[allow(unused)]
-pub struct UserDeleter {
-    limit:     Option<usize>,
-    order:     Option<aoba::Order>,
-    condition: UserCondition,
-} impl UserDeleter {
-    // ======
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub fn WHERE<F: Fn(UserCondition) -> UserCondition>(mut self, condition_builder: F) -> Self {
-        self.condition = condition_builder(self.condition);
-        self
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn password_eq<Str: aoba::AsStr>(mut self, another: Str) -> Self {
+            self.and(format!("password = '{}'", another.as_str()))
+        }
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn password_like<Str: aoba::AsStr>(mut self, regex: Str) -> Self {
+            self.and(format!("password LIKE '{}'", regex.as_str()))
+        }
     }
-    #[allow(non_snake_case)]
+
     #[allow(unused)]
-    pub fn LIMIT(mut self, limit: usize) -> Self {
-        self.limit = Some(limit);
-        self
+    pub struct UserCreater<
+        NameStr: aoba::AsStr,
+        PasswordStr: aoba::AsStr,
+    >(CreateUser<NameStr, PasswordStr>);
+    impl<
+        NameStr: aoba::AsStr,
+        PasswordStr: aoba::AsStr,
+    > UserCreater<NameStr, PasswordStr> {
+        pub(super) fn new(create_user: CreateUser<NameStr, PasswordStr>) -> Self {
+            Self(create_user)
+        }
+
+        // pub async fn exec<'e, E: sqlx::Executor<'e>>(self, executor: E) -> 
     }
-    #[allow(non_snake_case)]
+
     #[allow(unused)]
-    pub fn ORDER_ASC<F: Fn(UserFields) -> UserField>(mut self, field_selecter: F) -> Self {
-        let field = field_selecter(UserFields {
-            id: UserField::id,
-            name: UserField::name,
-            password: UserField::password,
-        });
-        self.order = Some(aoba::Order::Asc(field.as_field_name()));
-        self
+    pub struct UserSelecter {
+        limit:     Option<usize>,
+        order:     Option<aoba::Order>,
+        condition: UserCondition,
     }
-    #[allow(non_snake_case)]
+    impl UserSelecter {
+        #[allow(unused)]
+        pub(super) fn new(limit: Option<usize>) -> Self {
+            Self { limit, order: None, condition: UserCondition::new() }
+        }
+    }
+    impl UserSelecter {
+        // ======
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn WHERE<F: Fn(UserCondition) -> UserCondition>(mut self, condition_builder: F) -> Self {
+            self.condition = condition_builder(self.condition);
+            self
+        }
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn LIMIT(mut self, limit: usize) -> Self {
+            self.limit = Some(limit);
+            self
+        }
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn ORDER_ASC<F: Fn(UserFields) -> UserField>(mut self, field_selecter: F) -> Self {
+            let field = field_selecter(UserFields {
+                id: UserField::id,
+                name: UserField::name,
+                password: UserField::password,
+            });
+            self.order = Some(aoba::Order::Asc(field.as_field_name()));
+            self
+        }
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn ORDER_DESC<F: Fn(UserFields) -> UserField>(mut self, field_selecter: F) -> Self {
+            let field = field_selecter(UserFields {
+                id: UserField::id,
+                name: UserField::name,
+                password: UserField::password,
+            });
+            self.order = Some(aoba::Order::Desc(field.as_field_name()));
+            self
+        }
+        // =====
+    }
+
     #[allow(unused)]
-    pub fn ORDER_DESC<F: Fn(UserFields) -> UserField>(mut self, field_selecter: F) -> Self {
-        let field = field_selecter(UserFields {
-            id: UserField::id,
-            name: UserField::name,
-            password: UserField::password,
-        });
-        self.order = Some(aoba::Order::Desc(field.as_field_name()));
-        self
+    pub struct UserUpdater {
+        set_values: UpdateUser,
+        limit:      Option<usize>,
+        condition:  UserCondition,
     }
-    // =====
+    impl UserUpdater {
+        #[allow(unused)]
+        pub(super) fn new() -> Self {
+            Self { set_values: UpdateUser::new(), limit: None, condition: UserCondition::new() }
+        }
+    }
+    impl UserUpdater {
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn VALUES<F: Fn(UpdateUser) -> UpdateUser>(mut self, values_setter: F) -> Self {
+            self.set_values = values_setter(self.set_values);
+            self
+        }
+        // ======
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn WHERE<F: Fn(UserCondition) -> UserCondition>(mut self, condition_builder: F) -> Self {
+            self.condition = condition_builder(self.condition);
+            self
+        }
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn LIMIT(mut self, limit: usize) -> Self {
+            self.limit = Some(limit);
+            self
+        }
+        // #[allow(non_snake_case)]
+        // #[allow(unused)]
+        // pub fn ORDER_ASC<F: Fn(UserFields) -> UserField>(mut self, field_selecter: F) -> Self {
+        //     let field = field_selecter(UserFields {
+        //         id: UserField::id,
+        //         name: UserField::name,
+        //         password: UserField::password,
+        //     });
+        //     self.order = Some(aoba::Order::Asc(field.as_field_name()));
+        //     self
+        // }
+        // #[allow(non_snake_case)]
+        // #[allow(unused)]
+        // pub fn ORDER_DESC<F: Fn(UserFields) -> UserField>(mut self, field_selecter: F) -> Self {
+        //     let field = field_selecter(UserFields {
+        //         id: UserField::id,
+        //         name: UserField::name,
+        //         password: UserField::password,
+        //     });
+        //     self.order = Some(aoba::Order::Desc(field.as_field_name()));
+        //     self
+        // }
+        // =====
+    }
+
+    #[allow(unused)]
+    pub struct UserDeleter {
+        limit:     Option<usize>,
+        condition: UserCondition,
+    }
+    impl UserDeleter {
+        pub(super) fn new() -> Self {
+            Self { limit: None, condition: UserCondition::new() }
+        }
+    }
+    impl UserDeleter {
+        // ======
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn WHERE<F: Fn(UserCondition) -> UserCondition>(mut self, condition_builder: F) -> Self {
+            self.condition = condition_builder(self.condition);
+            self
+        }
+        #[allow(non_snake_case)]
+        #[allow(unused)]
+        pub fn LIMIT(mut self, limit: usize) -> Self {
+            self.limit = Some(limit);
+            self
+        }
+        // #[allow(non_snake_case)]
+        // #[allow(unused)]
+        // pub fn ORDER_ASC<F: Fn(UserFields) -> UserField>(mut self, field_selecter: F) -> Self {
+        //     let field = field_selecter(UserFields {
+        //         id: UserField::id,
+        //         name: UserField::name,
+        //         password: UserField::password,
+        //     });
+        //     self.order = Some(aoba::Order::Asc(field.as_field_name()));
+        //     self
+        // }
+        // #[allow(non_snake_case)]
+        // #[allow(unused)]
+        // pub fn ORDER_DESC<F: Fn(UserFields) -> UserField>(mut self, field_selecter: F) -> Self {
+        //     let field = field_selecter(UserFields {
+        //         id: UserField::id,
+        //         name: UserField::name,
+        //         password: UserField::password,
+        //     });
+        //     self.order = Some(aoba::Order::Desc(field.as_field_name()));
+        //     self
+        // }
+        // =====
+    }
+
 }
---
+++ src/experiment/user.rs
@@ -0,0 +1,25 @@
+use super::schema::{User, CreateUser};
+
+fn _user() {
+    let _ = User::ALL()
+        .LIMIT(100)
+        .ORDER_ASC(|user| user.name)
+        .WHERE(|user| user
+            .name_like("%user")
+            .id_between(1, 1000)
+        );
+
+    let _ = User::CREATE(CreateUser {
+        name: "user1",
+        password: "password",
+    });
+
+    let _ = User::UPDATE()
+        .VALUES(|user| user
+            .name("new user")
+            .password("new password")
+        )
+        .WHERE(|user| user
+            .id_eq(314)
+        );
+}
