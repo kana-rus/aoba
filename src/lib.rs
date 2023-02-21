98fa328ac225ba1937a5ae1ad8dadc0dfd266d7a
kana-rus <kanarus786@gmail.com>
2023-02-21 16:39:35 +0900

    editor completable

--- src/lib.rs
+++ src/lib.rs
@@ -4,56 +4,68 @@
 #![allow(non_snake_case, non_camel_case_types)]
 
 pub mod condition;
-pub mod db_type;
+pub mod schema;
 pub mod order;
 pub mod limit;
-
-pub use db_type::DBType;
 
 #[macro_export]
 macro_rules! schema {
     {
-        $(
+        // $(
             $( #[use $( $mixin:ident ),+] )?
             $model_name:ident {
                 $(
-                    $column_name:ident : $db_type:ident $( ($size:literal) )?
+                    $column_name:ident: $db_type:ident $(($size:literal))?
                     $( where
                         $first_column_constrain:ident
                         $( + $column_constrain:ident )*
-                    )?,
-                )*
-
-            }  
+                    )?
+                ),* $(,)?
+            }
             $( where
                 $first_table_constrain:ident $first_table_constrain_arg:tt
                 $( + $table_constrain:ident $table_constrain_arg:tt )*
             )?
-            ,
-        )*
+        // ),*
     } => {
-        // struct $model_name {
-        //     $(
-        //         $column_name: crate::DBType::$db_type $($size)? $()?
-        //     ),*
-        // }
+        // $(
+            #[allow(unused, non_upper_case_globals)]
+            mod $model_name {
+                $(
+                    const $column_name: crate::schema::DBType = crate::schema::DBType::$db_type $(($size))?;
+                )*
+            }
+        // )*
     };
 }
 
 schema!{
-    #[use id, times]
     User {
-        name:     VARCHAR(20),
-        password: VARCHAR(20) where NOT_NULL + UNIQUE,
-    } where
-        UNIQUE(name, password)
-        + CHECK(name IS NOT_NULL)
-        + CHECK(password IS NOT_NULL),
-
-    #[use id, times]
-    Task {
-        title:       VARCHAR(20) where NOT_NULL,
-        description: TEXT        where NOT_NULL,
-    },
+        name: TEXT,
+        password: VARCHAR(20)
+    }
 }
 
+// schema!{
+//     User {
+//         name: VARCHAR(20),
+//     }
+// }
+
+// schema!{
+//     #[use id, times]
+//     User {
+//         name:     VARCHAR(20),
+//         password: VARCHAR(20) where NOT_NULL + UNIQUE,
+//     } where
+//         UNIQUE(name, password)
+//         + CHECK(name IS NOT_NULL),
+// 
+//     #[use id, times]
+//     Task {
+//         title:       VARCHAR(20) where NOT_NULL,
+//         description: TEXT        where NOT_NULL,
+//     }
+// }
+// 
+// 
