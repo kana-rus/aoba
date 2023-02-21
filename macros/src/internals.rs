7ababdfc5be80dd7e7a2f910efe68ed7a1700695
kana-rus <kanarus786@gmail.com>
2023-02-21 21:57:52 +0900

    TODO: generate_orm

---
+++ macros/src/internals.rs
@@ -0,0 +1,7 @@
+use proc_macro2::TokenStream;
+use syn::Result;
+
+mod genarete_orm;
+pub(super) fn generate_orm(schema: TokenStream) -> Result<TokenStream> {
+
+}
--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,1 +1,9 @@
+mod internals;
+use proc_macro::TokenStream;
 
+#[proc_macro]
+pub fn generate_orm(schema: TokenStream) -> TokenStream {
+    internals::generate_orm(schema.into())
+        .unwrap_or_else(|err| err.into_compile_error())
+        .into()
+}
