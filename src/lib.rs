8cf5c6760a39eb83b781114240390825d11230b3
kana-rus <kanarus786@gmail.com>
2023-02-21 21:20:16 +0900

    Add: `any` type for constrains' arg

--- src/lib.rs
+++ src/lib.rs
@@ -15,10 +15,10 @@ macro_rules! schema {
             $( #[use $( $mixin:ident ),+] )?
             $model_name:ident {
                 $(
-                    $column_name:ident: $db_type:ident $(($size:literal))?
+                    $column_name:ident: $db_type:ident $( ($size:literal) )?
                     $( where
-                        $first_column_constrain:ident
-                        $( + $column_constrain:ident )*
+                        $first_column_constrain:ident $( ($first_column_constrain_arg:tt) )?
+                        $( + $column_constrain:ident $( ($column_constrain_arg:tt) )? )*
                     )?
                 ),* $(,)?
             }
@@ -32,11 +32,13 @@ macro_rules! schema {
             #[allow(unused, non_upper_case_globals)]
             mod $model_name {
                 $(
-                    const $column_name: crate::schema::DBType = crate::schema::DBType::$db_type $(($size))?;
+                    const $column_name: crate::schema::DBType = crate::schema::DBType::$db_type $( ($size) )?;
                     $(
-                        const _: crate::schema::ColumnConstrain = crate::schema::ColumnConstrain::$first_column_constrain;
+                        const _: crate::schema::ColumnConstrain
+                            = crate::schema::ColumnConstrain::$first_column_constrain $( (crate::schema::any(stringify!($first_column_constrain_arg))) )?;
                         $(
-                            const _: crate::schema::ColumnConstrain = crate::schema::ColumnConstrain::$column_constrain;
+                            const _: crate::schema::ColumnConstrain
+                                = crate::schema::ColumnConstrain::$column_constrain $( (crate::schema::any(stringify!($column_constrain_arg))) )?;
                         )*
                     )?
                 )*
@@ -50,29 +52,9 @@ schema!{
     User {
         name: TEXT,
         password: VARCHAR(20),
+    },
+    Task {
+        title: VARCHAR(20) where NOT_NULL + DEFAULT("No title"),
+        description: TEXT where NOT_NULL + DEFAULT(""),
     }
 }
-
-// schema!{
-//     User {
-//         name: VARCHAR(20),
-//     }
-// }
-
-// schema!{
-//     #[use id, times]
-//     User {
-//         name:     VARCHAR(20),
-//         password: VARCHAR(20) where NOT_NULL + UNIQUE,
-//     } where
-//         UNIQUE(name, password)
-//         + CHECK(name IS NOT_NULL),
-// 
-//     #[use id, times]
-//     Task {
-//         title:       VARCHAR(20) where NOT_NULL,
-//         description: TEXT        where NOT_NULL,
-//     }
-// }
-// 
-// 
--- src/schema/mod.rs
+++ src/schema/mod.rs
@@ -32,16 +32,28 @@ pub enum DBType {
 }
 
 pub enum ColumnConstrain {
-    CHECK(&'static str),
+    CHECK(any),
     NOT_NULL,
     UNIQUE,
     PRIMARY_KEY,
-    REFERENCES(&'static str),
-    DEFAULT(&'static str),
+    REFERENCES(any),
+    DEFAULT(any),
 }
 pub enum TableConstrain {
-    CHECK(&'static str),
-    UNIQUE(&'static str),
-    PRIMARY_KEY(&'static str),
-    FOREIGN_KEY(&'static str), REFERENCES(&'static str),
+    CHECK(any),
+    UNIQUE(any),
+    PRIMARY_KEY(any),
+    FOREIGN_KEY(any), REFERENCES(any),
 }
+
+/// Internal `&str` is automatically generated from input tokens, so you can input literally **any**thing in `()`.
+/// ```no_run
+/// schema!{
+///     User {
+///         name: TEXT where DEFAULT("No name"),
+///         age:  SMALLINT where DEFAULT(42)
+///          // Of course this is valid  ↑↑
+///     }
+/// }
+/// ```
+pub struct any(pub &'static str);
