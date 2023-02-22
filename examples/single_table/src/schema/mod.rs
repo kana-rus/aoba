0650258c3ff41fed99ac05533dd1295ff0405b8b
kana-rus <kanarus786@gmail.com>
2023-02-22 14:49:28 +0900

    save@2023-02-22 14:49+9:00

--- examples/single_table/src/schema/mod.rs
+++ examples/single_table/src/schema/mod.rs
@@ -3,8 +3,8 @@ use aoba::schema;
 schema!{
     #[use id, times]
     User {
-        name:     VARCHAR(20) where NOT_NULL + DEFAULT(""),
-        password: VARCHAR(30) where NOT_NULL + DEFAULT(""),
-    }
-    where CHECK(password != "password")
+        name:     VARCHAR(20) where NOT_NULL + DEFAULT("No name"),
+        password: VARCHAR(30) where NOT_NULL,
+    } where
+        CHECK(password != "password")
 }
